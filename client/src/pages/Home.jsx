ad49c9ab54bdf3543b4d111be5b5c5356ac66f27
Maestro1982 <kris.tote@telenet.be>
2023-02-02 09:23:56 +0100

    final commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,12 +23,15 @@ const Home = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/v1/post', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+      const response = await fetch(
+        'https://dall-e-7dye.onrender.com/api/v1/post',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();
